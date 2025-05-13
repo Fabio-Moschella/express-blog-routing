d0b38edddd0c2ded24d5319b6edf28b7c9c44648
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-13 17:36:59 +0200

    add-blogPostjs

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postRouter = require("./routers/posts.js");
+
+//STATIC ASSEST
 app.use(express.static("pubblic"));
 
 //ROUTERS
-app.use(postRouter);
+app.use("/post", postRouter);
 
 // IL SERVER E IN ASCOLTO SULLA PORTA 3000
 app.listen(port, () => {
